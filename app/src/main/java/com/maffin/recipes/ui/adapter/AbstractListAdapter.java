56e3c4ee5eb97eeff3174735fcf92444a6eb1231
Alexander Polyakov <zobr777@mail.ru>
2023-01-23 22:30:51 +0300

    Красивый вид элемента ListView в избранном

---
+++ app/src/main/java/com/maffin/recipes/ui/adapter/AbstractListAdapter.java
@@ -0,0 +1,227 @@
+package com.maffin.recipes.ui.adapter;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.core.content.ContextCompat;
+
+import com.maffin.recipes.R;
+import com.maffin.recipes.adapter.IGetItemPosition;
+import com.maffin.recipes.db.entity.Favorite;
+import com.maffin.recipes.ui.draw.VerticalImageSpan;
+
+import java.util.List;
+
+/**
+ * Абстрактный адаптер для заполнения данными элемента списков.
+ * Для переопределения вывода надо переопределить метод bindView().
+ */
+public abstract class AbstractListAdapter extends ArrayAdapter implements IGetItemPosition {
+    /** Ссылка на объек, конвертирующий шаблон в отображаемые элементы. */
+    private final LayoutInflater mInflater;
+    /** интерфейс к глобальной информации о среде приложений. */
+    private Context mContext;
+    /** Список с данными для адаптера. */
+    private List mData;
+
+    /**
+     * Конструктор.
+     *
+     * @param context интерфейс к глобальной информации о среде приложений.
+     * @param list    список с данными для адаптера
+     */
+    public AbstractListAdapter(final Context context, final List list) {
+        super(context, R.layout.favorite_list_item, R.id.receiptName, list);
+        mInflater = LayoutInflater.from(context);
+        mContext = context;
+        mData = list;
+    }
+
+    /**
+     * Возвращает контекст.
+     * @return  контекст
+     */
+    public Context getContext() {
+        return mContext;
+    }
+
+    /**
+     * Возвращает позицию элемента по его id.
+     *
+     * @param id идентификатор записи
+     * @return позиция элемента
+     */
+    @Override
+    public int getItemPosition(long id) {
+        return Long.valueOf(id).intValue();
+    }
+
+    /**
+     * Создает элементы списка.
+     * @param position  позиция в списке
+     * @param view      представление элемента
+     * @param parent    ссылка на родительский элемент
+     * @return  представление элемента
+     */
+    @Override
+    public View getView(final int position, final View view, final ViewGroup parent) {
+        View convertView;
+        if (view == null) {
+            convertView = newView(mContext, parent);
+        } else {
+            convertView = view;
+        }
+        bindView(position, convertView);
+        return convertView;
+    }
+
+    /**
+     * Возвращает список с данными.
+     * @return  список
+     */
+    public List getData() {
+        return mData;
+    }
+
+    /**
+     * Создает новый объект (элемент списка) для просмотра данных.
+     *
+     * @param context интерфейс к глобальной информации о среде приложений
+     * @param parent  ссылка на контейнер, в котором создается элемент списка, т.е. сам список
+     * @return созданный объект (элемент списка)
+     */
+    public final View newView(final Context context, final ViewGroup parent) {
+        // Создаем объект для хранения ссылок на составные части элемента списка
+        final ViewHolder holder = new ViewHolder();
+        // Создаем представление элемента списка по разметке
+        final View view = mInflater.inflate(R.layout.favorite_list_item, parent, false);
+        // Получаем ссылки на составные части элемента списка и сохранеям их в холдере
+        holder.mName = view.findViewById(R.id.receiptName);
+        holder.mTime = view.findViewById(R.id.receiptTime);
+        holder.mEnergy = view.findViewById(R.id.receiptEnergy);
+        holder.mThumbnail = (ImageView) view.findViewById(R.id.thumbnail);
+        holder.mDeleteFromFavorite = view.findViewById(R.id.deleteFromFavorite);
+        // Сохраняем ссылку на холдер в тэге представления
+        view.setTag(holder);
+        return view;
+    }
+
+    protected void spanImageIntoText(TextView textView, String atText, int imageId, int imgWidth, int imgHeight) {
+        String text = textView.getText().toString();
+        SpannableStringBuilder ssb = new SpannableStringBuilder(text);
+        Drawable drawable = ContextCompat.getDrawable(mContext, imageId);
+        drawable.mutate();
+        drawable.setBounds(0, 0, imgWidth, imgHeight);
+        int start = text.indexOf(atText);
+        VerticalImageSpan imageSpan = new VerticalImageSpan(drawable);
+        ssb.setSpan(imageSpan, start, start + atText.length(), Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
+        textView.setText(ssb, TextView.BufferType.SPANNABLE);
+    }
+
+    /**
+     * Биндит в шаблон элемента списка данные из массива.
+     *
+     * @param position позиция в списке
+     * @param view     Объект, соответствующий элементу списка
+     */
+    public void bindView(int position, View view) {
+        Context context = getContext();
+        // Получим ссылки на составные части шаблона
+        ViewHolder holder = (ViewHolder) view.getTag();
+        // Получим ссылку на рецепт (это элемент массива в соответствующей позиции)
+        final Favorite receipt = (Favorite) getData().get(position);
+        // Наполним строку данными
+        holder.setId(receipt.receiptId);
+        // Наименование рецепта
+        holder.getName().setText(receipt.receiptName);
+        // Время приготовления рецепта
+        int receiptTime = receipt.receiptTime;
+        if (receiptTime > 0) {
+            holder.getTime().setVisibility(View.VISIBLE);
+            holder.getTime().setText(context.getString(R.string.template_time, receiptTime));
+            spanImageIntoText(holder.getTime(), "[time-icon]",
+                    R.drawable.ic_baseline_access_time_24,
+                    context.getResources().getDimensionPixelOffset(R.dimen.icon_for_list_item),
+                    context.getResources().getDimensionPixelOffset(R.dimen.icon_for_list_item));
+        } else {
+            holder.getTime().setVisibility(View.GONE);
+        }
+        // Калорийность рецепта
+        int receiptKkal = receipt.receiptKkal;
+        if (receiptKkal > 0) {
+            holder.getEnergy().setVisibility(View.VISIBLE);
+            holder.getEnergy().setText(context.getString(R.string.template_energy, receiptKkal));
+            spanImageIntoText(holder.getEnergy(), "[energy-icon]",
+                    R.drawable.ic_baseline_fastfood_24,
+                    context.getResources().getDimensionPixelOffset(R.dimen.icon_for_list_item),
+                    context.getResources().getDimensionPixelOffset(R.dimen.icon_for_list_item));
+        } else {
+            holder.getEnergy().setVisibility(View.GONE);
+        }
+    }
+
+
+    /**
+     * Вспомогательный класс для хранения ссылок на элементы шаблона.
+     */
+    public class ViewHolder {
+        /** ID рецепта. */
+        private long 		mId;
+        /** Поле вывода наименования рецепта. */
+        private TextView 	mName;
+        /** Поле вывода времени приготовления рецепта. */
+        private TextView 	mTime;
+        /** Поле вывода кКал рецепта. */
+        private TextView 	mEnergy;
+        /** Миниатюра с картинкой рецепта. */
+        private ImageView   mThumbnail;
+        /** Иконка удаления из избранного. */
+        private ImageView   mDeleteFromFavorite;
+
+        /**
+         * Возвращает ID рецепта.
+         *
+         * @return ID рецепта
+         */
+        public long getId() {
+            return mId;
+        }
+
+        /**
+         * Устанавливает ID рецепта.
+         *
+         * @param id ID рецепта
+         */
+        public void setId(long id) {
+            mId = id;
+        }
+
+        public TextView getName() {
+            return mName;
+        }
+
+        public TextView getTime() {
+            return mTime;
+        }
+
+        public TextView getEnergy() {
+            return mEnergy;
+        }
+
+        public ImageView getThumbnail() {
+            return mThumbnail;
+        }
+
+        public ImageView getDeleteFromFavorite() {
+            return mDeleteFromFavorite;
+        }
+    }
+}
--- app/src/main/java/com/maffin/recipes/ui/favorite/FavoriteFragment.java
+++ app/src/main/java/com/maffin/recipes/ui/favorite/FavoriteFragment.java
@@ -1,32 +1,27 @@
 package com.maffin.recipes.ui.favorite;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.maffin.recipes.R;
-import com.maffin.recipes.adapter.IGetItemPosition;
 import com.maffin.recipes.databinding.FragmentFavoriteBinding;
 import com.maffin.recipes.db.entity.Favorite;
-
-import java.time.Duration;
-import java.util.List;
+import com.maffin.recipes.ui.adapter.FavoriteAdapter;
 
 /**
  * Фрагмент для отображения избранных рецептов.
  *
  * См. статья про обсерверы и LiveData: https://habr.com/ru/post/577482/
+ * См. статья про ListView: https://developer.alexanderklimov.ru/android/views/listview.php
+ * См. статью про RelativeLayoute: https://developer.alexanderklimov.ru/android/layout/relativelayout.php
+ * См. статью про кастомизацию элементов ListView: https://www.vogella.com/tutorials/AndroidListView/article.html
  */
 public class FavoriteFragment extends Fragment {
 
@@ -44,7 +39,8 @@ public class FavoriteFragment extends Fragment {
 
         // Инициализируем модель данных
         favoriteViewModel = new ViewModelProvider(this).get(FavoriteViewModel.class);
-        // Навешиваем прослушку на изменение данных в модели данных
+        // Навешиваем прослушку на изменение данных в модели данных. Когда модель получит данные из БД,
+        // прослушка через адаптер загрузит список в ListView
         final ListView listView = binding.listFavorite;
         favoriteViewModel.getList().observe(getViewLifecycleOwner(), favorites -> {
             ArrayAdapter<Favorite> adapter = new FavoriteAdapter(getContext(), favorites);
@@ -66,148 +62,5 @@ public class FavoriteFragment extends Fragment {
         super.onResume();
         // Загружаем данные из базы
         favoriteViewModel.loadData();
-    }
-
-    private class FavoriteAdapter extends ArrayAdapter<Favorite> implements IGetItemPosition {
-
-        /** Ссылка на объек, конвертирующий шаблон в отображаемые элементы. */
-        private final LayoutInflater 	mInflater;
-        /** интерфейс к глобальной информации о среде приложений. */
-        private Context 				mContext;
-        /** Список с данными для адаптера. */
-        private List<Favorite>          mData;
-
-        /**
-         * Конструктор.
-         *
-         * @param context
-         *            интерфейс к глобальной информации о среде приложений.
-         * @param list
-         *            список с данными для адаптера
-         */
-        public FavoriteAdapter(final Context context, final List<Favorite> list) {
-            super(context, R.layout.favorite_list_item, R.id.receiptName, list);
-            mInflater = LayoutInflater.from(context);
-            mContext = context;
-            mData = list;
-        }
-
-        /**
-         * Возвращает позицию элемента по его id.
-         *
-         * @param id
-         *            идентификатор записи
-         * @return позиция элемента
-         */
-        @Override
-        public int getItemPosition(long id) {
-            return Long.valueOf(id).intValue();
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public View getView(final int position, final View view, final ViewGroup parent) {
-            View convertView;
-            if (view == null) {
-                convertView = newView(mContext, parent);
-            } else {
-                convertView = view;
-            }
-            bindView(position, convertView);
-            return convertView;
-        }
-
-        /**
-         * Создает новый объект (элемент списка) для просмотра данных.
-         *
-         * @param context
-         *            интерфейс к глобальной информации о среде приложений
-         * @param parent
-         *            ссылка на контейнер, в котором создается элемент списка, т.е. сам список
-         * @return созданный объект (элемент списка)
-         */
-        public final View newView(final Context context, final ViewGroup parent) {
-            final ViewHolder holder = new ViewHolder();
-            final View view = mInflater.inflate(R.layout.favorite_list_item, parent, false);
-            holder.mName = (TextView) view.findViewById(R.id.receiptName);
-//            holder.mAnnotation = (TextView) view.findViewById(R.id.annotation);
-//            holder.mThumbnail = (ImageView) view.findViewById(R.id.thumbnail);
-//            holder.mCartSection = view.findViewById(R.id.cartSection);
-//            holder.mCartCount = (TextView) view.findViewById(R.id.cartCount);
-            view.setTag(holder);
-            return view;
-        }
-
-        /**
-         * Биндит в шаблон элемента списка данные из курсора.
-         *
-         * @param position
-         *            позиция в списке
-         * @param view
-         *            Объект, соответствующий элементу списка
-         */
-        public void bindView(final int position, final View view) {
-            // Получим ссылки на составные части шаблона
-            ViewHolder holder = (ViewHolder) view.getTag();
-            // Получим ссылку на рецепт
-            final Favorite receipt = mData.get(position);
-            // Наполним строку данными
-            holder.mName.setText(receipt.receiptName);
-//            final String annotation = reciept.getAnnotation();
-//            if (AdlRoutines.isEmpty(annotation)) {
-//                holder.mAnnotation.setVisibility(View.GONE);
-//            } else {
-//                holder.mAnnotation.setVisibility(View.VISIBLE);
-//                holder.mAnnotation.setText(Html.fromHtml(reciept.getAnnotation()));
-//            }
-//            holder.mId = new Integer(position).longValue();
-//            String photo = reciept.getThumbnail();
-//            if (AdlRoutines.isNotEmpty(photo)) {
-//                final Drawable d = AhLogic.loadImage(AhCategoryList.this, photo);
-//                if (d != null) {
-//                    holder.mThumbnail.setVisibility(View.VISIBLE);
-//                    holder.mThumbnail.setImageDrawable(d);
-//                }
-//            } else {
-//                holder.mThumbnail.setVisibility(View.GONE);
-//            }
-//            String key = mRecieptList.get(position);
-//            int cnt = AhLogic.getCart().getRecieptCount(key);
-//            if (cnt == 0) {
-//                holder.mCartCount.setVisibility(View.GONE);
-//            } else {
-//                holder.mCartCount.setVisibility(View.VISIBLE);
-//                holder.mCartCount.setText(Integer.toString(cnt));
-//            }
-        }
-
-        /**
-         * Вспомогательный класс для хранения ссылок на элементы шаблона.
-         */
-        private class ViewHolder {
-            /** ID записи. */
-            private long 		mId;
-            /** Поле вывода наименования рецепта. */
-            private TextView 	mName;
-//            /** Поле вывода краткого описания рецепта. */
-//            private TextView 	mAnnotation;
-//            /** Поле вывода краткого описания рецепта. */
-//            private ImageView 	mThumbnail;
-//            /** Секция добавления рецепта в корзину. */
-//            private View		mCartSection;
-//            /** Количество рецептов в корзине. */
-//            private TextView	mCartCount;
-
-            /**
-             * Возвращает ID записи.
-             *
-             * @return ID записи
-             */
-            public long getId() {
-                return mId;
-            }
-        }
     }
 }
